684ed76bc871166f35111315da45a57e9f54ae92
elfesyaesen <web.elfesyaesen@gmail.com>
2023-10-15 23:15:32 +0300

    first upload

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/userprofile', [AuthController::class, 'userProfile'])->middleware('jwt.verify');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'v1'
+], function ($router) {
+    Route::post('/login',       [AuthController::class, 'login']);
+    Route::post('/register',    [AuthController::class, 'register']);
+    Route::post('/logout',      [AuthController::class, 'logout']);
+    Route::post('/refresh',     [AuthController::class, 'refresh']);
+});
+
+Route::any('{any}', function(){
+    return response()->json([
+    	'status' => 'error',
+        'message' => 'Resource not found'], 404);
+})->where('any', '.*');
